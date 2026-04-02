c393980f89bfe2af7a4af63ca122d656af97bdf1
Gilberto Moreira <escolateologicavance@gmail.com>
2026-04-02 13:54:27 -0400

    Add fetch event listener to service worker
    Refactor service worker to handle fetch events and cache responses.

--- service-worker.js
+++ service-worker.js
@@ -20,13 +20,12 @@ const ASSETS = [
 ];
 
 // Instala e guarda os arquivos no cache
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
